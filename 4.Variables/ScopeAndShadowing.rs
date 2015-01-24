9734bd67ef94366e045fac1980b6866306724c00
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-01-24 18:03:17 +0900

    fix typo :lipstick:

--- 4.Variables/ScopeAndShadowing.rs
+++ 4.Variables/ScopeAndShadowing.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let long_lived_variable = 1i;
+    let long_lived_variable = 1is;
 
     {
-        let short_lived_variable = 2i;
+        let short_lived_variable = 2is;
 
         println!("inner short: {}", short_lived_variable);
-        let long_lived_valiable = 5_f32;
+        let long_lived_variable = 5_f32;
         println!("inner long: {}", long_lived_variable);
     }
 
