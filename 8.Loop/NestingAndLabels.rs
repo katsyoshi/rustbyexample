30e1d2285036e330042f431741b6b8e71f684d96
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-01-24 22:43:48 +0900

    add Nesting and labels

---
+++ 8.Loop/NestingAndLabels.rs
@@ -0,0 +1,12 @@
+fn main() {
+    'outer: loop {
+        println!("Entered the outer loop");
+        'inner: loop {
+            println!("Entered the inner loop");
+            // break;
+            break 'outer;
+        }
+        println!("This point will never be reached");
+    }
+    println!("Exited the outer loop");
+}
