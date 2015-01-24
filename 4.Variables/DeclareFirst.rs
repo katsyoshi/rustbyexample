04c73e2e279f4698f7d063fa435644127231038d
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-01-24 18:07:21 +0900

    add DeclareFirst

---
+++ 4.Variables/DeclareFirst.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let a_variable;
+
+    {
+        let x = 2i;
+
+        a_variable = x * x;
+    }
+
+    println!("a variable: {}", a_variable);
+
+    let another_variable;
+
+    println!("another variable: {}", another_variable);
+
+    another_variable = 1i;
+
+    println!("another variable: {}", another_variable);
+}
