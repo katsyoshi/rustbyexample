046ccd74f601a9f6bb783d7afc9b50099af58f9f
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-01-24 17:53:07 +0900

    add scope and shadowning

---
+++ 4.Variables/ScopeAndShadowing.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let long_lived_variable = 1i;
+
+    {
+        let short_lived_variable = 2i;
+
+        println!("inner short: {}", short_lived_variable);
+        let long_lived_valiable = 5_f32;
+        println!("inner long: {}", long_lived_variable);
+    }
+
+    println!("outer short: {}", short_lived_variable);
+
+    println!("outer long: {}", long_lived_variable);
+}
